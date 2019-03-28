578c29744849cc57fe45bef18e5666b271eb0535
aashishkr <aashishkumar96.akr@gmail.com>
2019-03-28 08:52:16 +0530

    machine-learning-ex6 completed

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,10 +23,24 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
-
-
-
-
+x1 = [1 2 1]; x2 = [0 4 -1];
+C_Set = [0.01 0.03 0.1 0.3 1 3 10 30];
+sigma_Set  = [0.01 0.03 0.1 0.3 1 3 10 30];
+lenC = length(C_Set);
+lenSigma = length(sigma_Set);
+m = Inf;
+for i= 1: lenC
+  for j= 1:lenSigma
+    model= svmTrain(X, y, C_Set(i), @(x1, x2) gaussianKernel(x1, x2, sigma_Set(j)));
+    predictions = svmPredict(model, Xval);
+    mean1 =   mean(double(predictions ~= yval));
+    if mean1<m
+       m = mean1;
+       C= C_Set(i);
+       sigma = sigma_Set(j);
+    end
+  end
+end
 
 
 % =========================================================================
